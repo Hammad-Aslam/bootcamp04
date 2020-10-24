51fe5601d19d76d5eb1668a78f9f991d3ea1e091
Hammad Aslam <hammadaslam382@gmail.com>
2020-10-24 11:43:06 +0500

    add bootcamp project 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import Message from './message.js';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function App(){
+  let [isCount, setCount] = useState(1);
+  let [isDay, setDay] = useState(true,false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="day">
+      <Message counter = {isCount}/>
+      <h1> 
+        This is bootcamp session {isCount}
+      </h1>
+      <button onClick={
+        ()=> setCount(++isCount)
+      }>
+        Click!
+      </button>
+      <h1>
+       This line tells us a weekend days {isDay ? 'Saturday': 'Sunday'}
+      </h1>
+      <button on onClick={
+        ()=> setDay(!isDay)
+      }>
+        Day
+      </button>
     </div>
-  );
-}
-
+    
+  )
+};
 export default App;
---
+++ src/message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Message(props){
+    return(
+    <h3 className="night">This line tells a Bootcamp session {props.counter}</h3>
+    )
+}
+
+export default Message;
